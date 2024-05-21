b65b12a97d2e28134e47369d3b08dd77ad0f5b43
Namima Islam <namimaislam308@gmail.com>
2024-05-21 14:10:23 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
